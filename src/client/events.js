7c308b17b5c55354c058097a0f4226c23a1d0775
Bojan Hodzic <bojan.hodzic@192.com>
2016-06-25 15:02:47 +0200

    Clear values

--- src/client/events.js
+++ src/client/events.js
@@ -13,10 +13,14 @@ Template.home.events({
             rd.onload = function(e) {
                 var cols = e.target.result.split("\n").map(i => i.split(","));
 
-                Meteor.call('sendEmail', cols[0], "test@zamphyr.com", $("#js-subject").val(), $("#js-markdown").val(), (err, data) => {
+                Meteor.call('sendEmail', cols[0], "test@zamphyr.com", $("#js-subject").val(), $("#js-message").val(), (err, data) => {
                     if(!err)
                         document.getElementById("js-submit-btn").disabled = false;
                         $("#js-submit-btn").text("Send");
+                        $("#js-subject").val("");
+                        $("#js-message").val("");
+                        $("#js-csvMail").val("");
+                       
                 });
             }
 
